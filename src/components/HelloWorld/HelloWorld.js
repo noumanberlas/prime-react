1b188710dbc9c20dfe4b591f9ed8d64312d95f01
Nouman Berlas <noumanberlas@gmail.com>
2017-09-17 17:01:58 +0500

    docs gen

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function HelloWorld({message}){
+    return <div>Hello {message}</div>
+}
+
+HelloWorld.propTypes = {
+    message : PropTypes.string
+}
+
+export default HelloWorld;
