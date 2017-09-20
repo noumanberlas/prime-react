65d18e728f504ac37b01b08bce315fc4ead54e56
Nouman Berlas <noumanberlas@gmail.com>
2017-09-20 21:46:33 +0500

    Styling done

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -168,6 +168,8 @@ module.exports = {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
+                //  module:true,
+                //  localIdentName :'[name]_[local]_[hash:base64:5]'
                 },
               },
               {
